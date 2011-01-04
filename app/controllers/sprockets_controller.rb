979f0c99266383bdb998bd5a341acc56e2432387
Marian Theisen <marian@cice-online.net>
2011-01-04 13:01:45 +0100

    boeser fehler

--- app/controllers/sprockets_controller.rb
+++ app/controllers/sprockets_controller.rb
@@ -1,15 +1,10 @@
 class SprocketsController < ActionController::Base
   respond_to :js
-  caches_page :show, :if => proc(&:perform_caching?)
+  caches_page :show, :if => proc {Rails.configuration.action_controller.perform_caching}
   
   def show
     sprocket_set = SprocketSet.new params[:id]
     
     respond_with sprocket_set
-  end
-  
-  protected
-  def perform_caching?
-    Rails.configuration.action_controller.perform_caching
   end
 end
